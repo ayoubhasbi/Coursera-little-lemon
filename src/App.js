8a154281719b16727aaa3a9dbb99cf0fdbe7a773
ayoub <ayoubwask7@gmail.com>
2023-03-17 03:07:17 +0100

    add src folder

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
